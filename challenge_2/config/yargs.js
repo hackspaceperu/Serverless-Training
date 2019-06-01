7b442aebf6b639c3739798108bffa3ad174790ec
UsuarioTemporal <thomtwd@gmail.com>
2019-06-01 12:53:44 -0500

    Configuracion del yargs

--- challenge_2/config/yargs.js
+++ challenge_2/config/yargs.js
@@ -1,5 +1,17 @@
 import argv from 'yargs'
 
 const optns = {
-    
+    description:{
+        demand:true,
+        alias:'d'
+    }
 }
+
+argv.command('create_artist','Creacion de un artista',optns)
+    .command('create_musical_genre','Creacion de un genero musical',optns)
+    .command('create_album','Creacion de un genero musical',optns)
+    .command('create_song','Creacion de un genero musical',optns)
+    .help()
+    .argv
+
+module.exports = argv
