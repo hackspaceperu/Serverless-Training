8d3ba8536998d8c3c421527fb3f48b7bd3069ec6
UsuarioTemporal <thomtwd@gmail.com>
2019-05-31 23:26:49 -0500

    Haciendo pruebas antes del reto

---
+++ challenge_2/test/test.js
@@ -0,0 +1,4 @@
+import {hola} from './test_2'
+let m = process.argv[2].split('=')[1]
+hola('sda')
+// npm run dev test/test  s=23
---
+++ challenge_2/test/test_2.js
@@ -0,0 +1,3 @@
+export const hola = (msg='un mensaje')=>{
+    console.log(msg);
+}
