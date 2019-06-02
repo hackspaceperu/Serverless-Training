7a9f6277d88a49b946077ec2d17c2910023d07c2
UsuarioTemporal <thomtwd@gmail.com>
2019-06-01 19:07:13 -0500

    Creacion de entidades mediante el patron factory

--- challenge_2/src/FactoryEntity.js
+++ challenge_2/src/FactoryEntity.js
@@ -6,7 +6,9 @@ const Artist = require('./Artist'),
 module.exports =class FactoryEntity{
     get entity(argv){
         //logica
-        if(argv._[0]==='create_artist') return new Artist({})
-        
+        if(argv._[0]==='create_artist') return new Artist({id:argv.id,dni:argv.dni,name:argv.name,email:argv.email})
+        if(argv._[0]==='create_musical_genre') return new MusicalEntity({id:argv.id,type:argv.type})
+        if(argv._[0]==='create_album') return new Album({})
+        if(argv._[0]==='create_song') return new Song({})
     }
 }
--- challenge_2/src/Spotify.js
+++ challenge_2/src/Spotify.js
@@ -1,4 +1,2 @@
 const {yargs} = require('../config/yargs')
 // import colors from 'colors'
-const argumnets = yargs
-console.log(argumnets)
