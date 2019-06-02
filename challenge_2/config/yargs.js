5afde687cdd3112b7f47e119b3ba654059bb0603
UsuarioTemporal <thomtwd@gmail.com>
2019-06-01 23:36:27 -0500

    Delete

--- challenge_2/config/yargs.js
+++ challenge_2/config/yargs.js
@@ -4,17 +4,8 @@ const id = {
     alias:'i',
     demand:true
 }
-const to_list_artists = {}
-const show_artist_by_id ={id}
-
-const to_list_songs = {}
-const show_song_by_id ={id}
-
-const to_list_albums = {}
-const show_album_by_id ={id}
-
-const to_list_gs ={}
-const show_genre_by_id={id}
+const to_list = {}
+const by_id ={id}
 
 const create_artist={
     id:{
@@ -84,14 +75,18 @@ const yargs = argv.command('create_artist','Creacion de un artista',create_artis
                 .command('create_musical_genre','Creacion de un genero musical',create_musical_genre)
                 .command('create_album','Creacion de un album',create_album)
                 .command('create_song','Creacion de una cancion',create_song)
-                .command('to_list_artists','Listar artistas',to_list_artists)
-                .command('show_artist_by_id','Mostrar artista por id',show_artist_by_id)
-                .command('to_list_songs','Listar canciones',to_list_songs)
-                .command('show_song_by_id','Mostrar cancion por id',show_song_by_id)
-                .command('to_list_albums','Listar albumes',to_list_albums)
-                .command('show_album_by_id','Mostrar album por id',show_album_by_id)
-                .command('to_list_gs','Listar generos',to_list_gs)
-                .command('show_genre_by_id','Mostrar genero por id',show_genre_by_id)
+                .command('to_list_artists','Listar artistas',to_list)
+                .command('show_artist_by_id','Mostrar artista por id',by_id)
+                .command('to_list_songs','Listar canciones',to_list)
+                .command('show_song_by_id','Mostrar cancion por id',by_id)
+                .command('to_list_albums','Listar albumes',to_list)
+                .command('show_album_by_id','Mostrar album por id',by_id)
+                .command('to_list_gs','Listar generos',to_list)
+                .command('show_genre_by_id','Mostrar genero por id',by_id)
+                .command('delete_by_id_song','Eliminar cancion por id',by_id)
+                .command('delete_by_id_artist','Eliminar artista por id',by_id)
+                .command('delete_by_id_album','Eliminar album por id',by_id)
+                .command('delete_by_id_gn','Eliminar genero por id',by_id)
                 .help()
                 .argv
 
--- challenge_2/src/crud.js
+++ challenge_2/src/crud.js
@@ -30,6 +30,7 @@ const create = argv=>{
     if(argv._[0]==='create_musical_genre') db.musical_genres.push(entity)
     if(argv._[0]==='create_album') db.albums.push(entity)
     if(argv._[0]==='create_song') db.songs.push(entity)
+    // print(db)
     saveDB().then(data=>print(entity)).catch(console.error)
 }
 // loadingDB()
@@ -48,11 +49,67 @@ const showById = argv =>{
     if(argv._[0]==='show_album_by_id') return print(db.albums.filter(al=>al.id===argv.id))
     if(argv._[0]==='show_genre_by_id') return print(db.musical_genres.filter(m=>m.id===argv.id))
 }
+const deleteById = argv =>{
+    loadingDB()
+    if(argv._[0]==='delete_by_id_artist') {
+        let index_artist = db.artists.filter((art,index)=>{
+            if(art.id===argv.id) return index
+        })[0]
+        let index_album = db.albums.filter((alb,index)=>{
+            if(alb.idArtist===argv.id) return index
+        })
+        let index_song =db.songs.filter((so,index)=>{
+            if(so.idAlbum===argv.id) return index
+        })
+        db.artists.splice(index_artist,1)
+        
+        index_album.forEach(item=>{
+            db.albums.splice(item,1)
+        })
+        index_song.forEach(item=>{
+            db.songs.splice(item,1)
+        })
+    }
+    else if(argv._[0]==='delete_by_id_song'){
+        let index_song =db.songs.filter((so,index)=>{
+            if(so.id===argv.id) return index
+        })[0]
+        db.songs.splice(index_song,1)
+    }
+    else if(argv._[0]==='delete_by_id_album'){
+        let index_album = db.albums.filter((alb,index)=>{
+            if(alb.idArtist===argv.id) return index
+        })[0]
+        let index_song =db.songs.filter((so,index)=>{
+            if(so.idAlbum===argv.id) return index
+        })
+        
+        db.albums.splice(index_album,1)
+        index_song.forEach(item=>{
+            db.songs.splice(item,1)
+        })
+        
+    }
+    else if(argv._[0]==='delete_by_id_gn') {
+        let index_ge = db.musical_genres.filter((g,index)=>{
+            if(g.id===argv.id) return index
+        })[0]
+        let index_song =db.songs.filter((so,index)=>{
+            if(so.idGenre===argv.id) return index
+        })
+        
+        db.musical_genres.splice(index_ge,1)
+        index_song.forEach(item=>{
+            db.songs.splice(item,1)
+        })
+    }
+    saveDB().then(console.log).catch(console.error)
+}
 // let e = f.buildEntity(yargs)
 // db.artists.push(e)
 // db.artists.push(e)
-
+// create(yargs)
+// deleteById(yargs)
 // console.log(db.artists)
 // toListOf(yargs)
-showById(yargs)
-
+// showById(yargs)
