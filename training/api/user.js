21901761ef7422f4dc69b12283a58cad0abdb15d
MaurickThom <thomtwd@gmail.com>
2019-06-09 16:06:23 -0500

    Agregando paginacion del api

--- training/api/user.js
+++ training/api/user.js
@@ -7,9 +7,11 @@ router.get('/',(req,res)=>{
     let query = req.query
     if(!Object.keys(query).length) return res.json(db)
     let id = +req.query.id
+
     // console.log(id);
-    res.json(db[id-1])
+    // res.json(db[id-1])
     // res.send(req.body)
+    res.json([...db].splice(0,req.query.limit || db.length))
 })
 //params
 // localhost:port/user/1
@@ -17,7 +19,8 @@ router.get('/',(req,res)=>{
 // localhost:port/user/1/products/54
 router.get('/:id',(req,res)=>{
     // console.log(req.params.id);
-    res.json(db[+req.params.id-1])
+    // res.json(db[+req.params.id-1])
+    res.json(db.find((user)=>+req.params.id===user.id))
 })
 router.post('/',(req,res)=>{
     res.send(req.body)
