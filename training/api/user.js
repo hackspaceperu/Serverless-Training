e68f8154029856a6a4028dc1aa63275bb34ed498
MaurickThom <thomtwd@gmail.com>
2019-06-09 15:59:37 -0500

    Jugando con los query params

--- training/api/user.js
+++ training/api/user.js
@@ -1,11 +1,30 @@
 const express = require('express'),
-    router = express.Router()
+    router = express.Router(),
+    db = require('../db.json')
 
 router.get('/',(req,res)=>{
-    res.json(require('../db.json'))
+    
+    let query = req.query
+    if(!Object.keys(query).length) return res.json(db)
+    let id = +req.query.id
+    // console.log(id);
+    res.json(db[id-1])
+    // res.send(req.body)
+})
+//params
+// localhost:port/user/1
+// localhost:port/user/1/products
+// localhost:port/user/1/products/54
+router.get('/:id',(req,res)=>{
+    // console.log(req.params.id);
+    res.json(db[+req.params.id-1])
 })
 router.post('/',(req,res)=>{
     res.send(req.body)
 })
 
 module.exports = router
+
+//params
+//body
+//query
